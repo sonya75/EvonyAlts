318698bcfd7f70ce9c3a6c9ab243ccb64da7e4af
Sonya Roy <sonyaroy75@gmail.com>
2017-01-14 21:33:33 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-import os
-os.system("cd tor-0.2.9.8;./configure && make")
+import urllib2
+for i in range(0,2000):
+  try:
+    urllib2.urlopen('http://e2.eimg.us/EvonyClient'+str(i)+".swf")
+    print ("Working "+str(i))
+  except:
+    print ("Not Working "+str(i))
