62cdd863e5678d514d6619a2704b171725e99d59
Sonya Roy <sonyaroy75@gmail.com>
2017-01-14 21:37:24 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 import urllib2
+import time
 for i in range(0,2000):
   try:
     urllib2.urlopen('http://e2.eimg.us/EvonyClient'+str(i)+".swf")
     print ("Working "+str(i))
   except:
     print ("Not Working "+str(i))
+    time.sleep(1)
