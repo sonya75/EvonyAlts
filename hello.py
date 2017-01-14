3e0e57eb22bab2f34354251782ffd0a2b40dc26d
Sonya Roy <sonyaroy75@gmail.com>
2017-01-14 10:48:18 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
 import os
-os.system("sudo mount -o remount,rw /")
-os.system("wget https://www.torproject.org/dist/tor-0.2.9.8.tar.gz")
-os.system("tar xzf tor-0.2.9.8.tar.gz; cd tor-0.2.9.8")
-os.system("./configure && make")
+os.system("./tor-0.2.9.8/configure")
+os.system("/tor-0.2.9.8/make")
